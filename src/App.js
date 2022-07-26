ca50a837d1a2e03dc5ff5ee3aa08c937ef48be9c
Jimmy Zheng <jimmyz0622@gmail.com>
2022-07-25 22:01:48 -0700

    add menu to site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import KingNoodleMenu from "./KingNoodleHouse-July-25-2022.pdf";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <iframe
+                src={`${KingNoodleMenu}#view=fitH&toolbar=0`}
+                title="KingNoodleHouse"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                allowFullScreen=""
+                style={{ position: "absolute", top: 0, left: 0 }}
+            />
+        </div>
+    );
 }
 
 export default App;
